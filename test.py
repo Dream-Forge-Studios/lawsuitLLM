e76d9e22e17d7b387280a8316aefde3f5f9daae2
tyflow <jshi25@naver.com>
2024-02-14 12:11:28 +0900

    test

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ def stream(user_prompt):
 
     streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
-    generated_ids = model.generate(**inputs, streamer=streamer, max_new_tokens=200)
+    generated_ids = model.generate(**inputs, streamer=streamer, max_new_tokens=4096)
 
     decoded = tokenizer.batch_decode(generated_ids)
     # 질문과 답변 분리
@@ -24,19 +24,24 @@ def stream(user_prompt):
 
 
 model = AutoModelForCausalLM.from_pretrained(
-    "maywell/Synatra-7B-v0.3-dpo", low_cpu_mem_usage=True,
+    "/data/Synatra-7B-v0.3-dpo", low_cpu_mem_usage=True,
     return_dict=True,torch_dtype=torch.bfloat16,
     device_map= {"": 0})
 
 # LORA로 미세조정한 모델을 로드합니다.
-# new_model = "/data/llm/lawsuit-7B-easylaw_kr-e3"
+new_model = "/data/llm/lawsuit-7B-civil-wage-a"
 # new_model = r"D:\lawsuit-7B-easylaw_kr-e3"
-# model = PeftModel.from_pretrained(model, new_model)
+model = PeftModel.from_pretrained(model, new_model)
 
 # tokenizer = AutoTokenizer.from_pretrained("/data/llm/Synatra-7B-v0.3-dpo")
-tokenizer = AutoTokenizer.from_pretrained("maywell/Synatra-7B-v0.3-dpo")
+tokenizer = AutoTokenizer.from_pretrained("/data/Synatra-7B-v0.3-dpo")
 
-stream("신호를 어겨서 벌점을 받았는데 이거는 평생가는거야?")
+stream("판결 요지: '지방자치단체의 장의 보조기관인 건설과장의 요청에 의하여 노무자가 일정한 노무를 제공한 경우, 이 계약의 체결이 위 과장의 권한에 속하는 한 이는 유효하게 성립한 것으로 위 과장이 노무자를 고용함에 있어서 행정상의 내부적인 절차를 경유하지 않았다고 할지라도 그것은 행정상의 내부관계에 불과하여 이로써 위 노무계약이 위법이거나 무효라고 단정할 수 없다.' "
+       "참조 조문: 민법 제655조 "
+       "판결 요지: '근로자가 유급휴일에 근로한 경우에는 유급으로서 당연히 지급되는 임금과 그 유급휴일의 근로에 대한 소정의 통상임금을 포함한 임금을 지급하여야 된다.' "
+       "참조 조문: 근로기준법 제47조 "
+       "판결 요지: '피고회사가 근로자들을 월남국으로 파병시킬 당시 미국인회사로부터 앞으로 일년간 공사시공을 함에 상당한 하도급을 받았으나 그 후의 사정변경으로 하도급받은 작업량이 줄어들게 되었다는 사실로써는 민법 제661조나 본조 제1항 단서 소정 부득이한 사유라 할 수 없으므로 해고기간의 정함이 있는 위 근로자들과의 본건 해고계약은 30일 전의 예고로도 해지시킬 수 없다.' "
+       "참조 조문:")
 
 # class BankAccount:
 #     def __init__(self, account: str):
