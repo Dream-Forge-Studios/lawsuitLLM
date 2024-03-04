2ef3282ecdfa5c14e6348e1d2d71367c4a9f071f
tyflow <jshi25@naver.com>
2024-03-04 16:18:56 +0900

    wage-100-c

--- pretrain.py
+++ pretrain.py
@@ -14,7 +14,7 @@ from datasets import Dataset
 # base_model = "maywell/Synatra-7B-v0.3-dpo"
 base_model = "/data/llm/Synatra-7B-v0.3-dpo"
 # base_model = "D:\Synatra-7B-v0.3-dpo"
-dataset_name, new_model = "joonhok-exo-ai/korean_law_open_data_precedents", "/data/llm/lawsuit-7B-wage500-QA100-c"
+dataset_name, new_model = "joonhok-exo-ai/korean_law_open_data_precedents", "/data/llm/lawsuit-7B-wage-100-c"
 # dataset_name2 = 'maywell/korean_textbooks'
 dataset_name2 = 'maywell/ko_wikidata_QA'
 
@@ -82,10 +82,10 @@ dataset = load_dataset(dataset_name, cache_dir=custom_cache_dir, split="train")
 # '민사' 사건 중 '임금'만 포함된 데이터 필터링하면서 테스트 케이스 제외
 civil_cases_with_wage_excluded = dataset.filter(
     lambda x: x['사건종류명'] == '민사' and
-              x['사건명'] is not None and
-              '임금' in x['사건명']
+              # x['사건명'] is not None and
+              # '임금' in x['사건명']
               # and
-              # (str(x['판례정보일련번호']) in test_case_numbers or (x['사건명'] is not None and '임금' in x['사건명']))
+              (str(x['판례정보일련번호']) in test_case_numbers or (x['사건명'] is not None and '임금' in x['사건명']))
               # x['참조조문'] is not None
               # str(x['판례정보일련번호']) in test_case_numbers
               # str(x['판례정보일련번호']) not in test_case_numbers
@@ -100,16 +100,16 @@ civil_cases_with_wage_excluded = dataset.filter(
 processed_dataset = civil_cases_with_wage_excluded.map(precedents_preprocess_data)
 # #
 # dataset2 = load_dataset(dataset_name2, 'claude_evol', cache_dir=custom_cache_dir, split="train")
-dataset2 = load_dataset(dataset_name2, cache_dir=custom_cache_dir, split="train")
-random_samples = dataset2.select(range(100))
-
-qa_dataset = random_samples.map(preprocess_data)
-
-combined_dataset = concatenate_datasets([processed_dataset, qa_dataset]).shuffle()
+# dataset2 = load_dataset(dataset_name2, cache_dir=custom_cache_dir, split="train")
+# random_samples = dataset2.select(range(100))
+#
+# qa_dataset = random_samples.map(preprocess_data)
+#
+# combined_dataset = concatenate_datasets([processed_dataset, qa_dataset]).shuffle()
 
 # 원본 데이터셋의 다른 열을 제거하고 'input_text'만 남깁니다.
-# final_dataset = processed_dataset.remove_columns([column_name for column_name in processed_dataset.column_names if column_name != 'input_text'])
-final_dataset = combined_dataset.remove_columns([column_name for column_name in combined_dataset.column_names if column_name != 'input_text'])
+final_dataset = processed_dataset.remove_columns([column_name for column_name in processed_dataset.column_names if column_name != 'input_text'])
+# final_dataset = combined_dataset.remove_columns([column_name for column_name in combined_dataset.column_names if column_name != 'input_text'])
 # 데이터셋 토큰화 함수
 def tokenize_function(examples):
     return tokenizer(examples['input_text'], truncation=True, padding=True, max_length=cutoff_len)
