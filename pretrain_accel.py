291c20c96e3fc0bef20a09655abdcd933494e9c0
tyflow <jshi25@naver.com>
2024-04-03 16:21:19 +0900

    remove_columns

--- pretrain_accel.py
+++ pretrain_accel.py
@@ -32,7 +32,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 
 # base_model = "maywell/Synatra-7B-v0.3-dpo"
 base_model = "/data/llm/Synatra-7B-v0.3-dpo"
-base_model = "D:\Synatra-7B-v0.3-dpo"
+# base_model = "D:\Synatra-7B-v0.3-dpo"
 
 # accelerator
 # fsdp_plugin = FullyShardedDataParallelPlugin(
